b81f66f30f628bdb86daeef5d4279266b3ed23a9
Anders Borch <anders@cyborch.com>
2019-04-27 09:12:55 +0200

    Added activity model

---
+++ SmartSleep/Services/Activity/ActivityProgressUpdate.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityProgressUpdate.swift
+//  SmartSleep
+//
+//  Created by Anders Borch on 17/03/2019.
+//  Copyright © 2019 Anders Borch. All rights reserved.
+//
+
+import Foundation
+
+struct ActivityProgressUpdate {
+    let activity: Activity?
+    let remaining: Int
+}
+
