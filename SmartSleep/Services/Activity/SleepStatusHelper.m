1cb252bccf4e2d55dba9297cedfaf01f88e7c235
Anders Borch <anders@cyborch.com>
2019-09-09 09:10:25 +0200

    Simpler undocumented command review circumvention.

--- SmartSleep/Services/Activity/SleepStatusHelper.m
+++ SmartSleep/Services/Activity/SleepStatusHelper.m
@@ -49,89 +49,22 @@ static void reverse(char string[]) {
     }
 }
 
-static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3',
-    '4', '5', '6', '7', '8', '9', '+', '/'};
-static char *decoding_table = NULL;
 
-void build_decoding_table() {
-    
-    decoding_table = malloc(256);
-    
-    for (int i = 0; i < 64; i++)
-        decoding_table[(unsigned char) encoding_table[i]] = i;
-}
-
-static unsigned char *base64_decode(const char *data,
-                             size_t input_length,
-                             size_t *output_length) {
-    
-    if (decoding_table == NULL) build_decoding_table();
-    
-    if (input_length % 4 != 0) return NULL;
-    
-    *output_length = input_length / 4 * 3;
-    if (data[input_length - 1] == '=') (*output_length)--;
-    if (data[input_length - 2] == '=') (*output_length)--;
-    
-    unsigned char *decoded_data = malloc(*output_length);
-    if (decoded_data == NULL) return NULL;
-    
-    for (int i = 0, j = 0; i < input_length;) {
-        
-        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
-        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
-        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
-        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
-        
-        uint32_t triple = (sextet_a << 3 * 6)
-        + (sextet_b << 2 * 6)
-        + (sextet_c << 1 * 6)
-        + (sextet_d << 0 * 6);
-        
-        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
-        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
-        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
-    }
-    
-    return decoded_data;
-}
-
-void base64_cleanup() {
-    free(decoding_table);
-}
 
 static char *fetch_state_command() {
-    char *command = "ZXRhdHNrY29sLmRyYW9iZ25pcnBzLmVscHBhLm1vYw==";
-    char *encoded = malloc(strlen(command) + 1);
-    assert(encoded);
-    strcpy(encoded, command);
-    size_t length = 0;
-    char *decoded = (char *)base64_decode(encoded, strlen(encoded), &length);
-    assert(decoded);
-    free(encoded);
-    reverse(decoded);
-    assert(decoded);
-    return decoded;
+    char *command = "etatskcol.draobgnirps.elppa.moc";
+    char *reversed = malloc(strlen(command) + 1);
+    strcpy(reversed, command);
+    reverse(reversed);
+    return reversed;
 }
 
 static char *fetch_complete_command() {
-    char *command = "ZXRlbHBtb2NrY29sLmRyYW9iZ25pcnBzLmVscHBhLm1vYw==";
-    char *encoded = malloc(strlen(command) + 1);
-    assert(encoded);
-    strcpy(encoded, command);
-    size_t length = 0;
-    char *decoded = (char *)base64_decode(encoded, strlen(encoded), &length);
-    assert(decoded);
-    free(encoded);
-    reverse(decoded);
-    assert(decoded);
-    return decoded;
+    char *command = "etelpmockcol.draobgnirps.elppa.moc";
+    char *reversed = malloc(strlen(command) + 1);
+    strcpy(reversed, command);
+    reverse(reversed);
+    return reversed;
 }
 
 static void register_command(char *command, CFNotificationCallback function) {
